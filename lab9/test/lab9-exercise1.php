523b9d5cecf2ec4f339e172bd2663b661436760c
2449278392@qq.com <55231475+yoyazhang@users.noreply.github.com>
2020-05-28 09:44:23 +0800

    lab9 upload1

--- lab9/test/lab9-exercise1.php
+++ lab9/test/lab9-exercise1.php
@@ -9,7 +9,14 @@
 <h1>PHP section (inside &lt;?php ... ?&gt; tags)</h1>
 <?php
 //this is a php comment IN tags (will not appear)
-echo "This was output using PHP";
+$isLeapYear = date("L");
+if($isLeapYear === 1){
+    $remaining = 366-date("z");
+}
+else{
+    $remaining = 365-date("z");
+}
+echo "There are ". $remaining . " days left in the year";
 echo "<br>"; //notice we must echo tags in php.
 
 ?>
